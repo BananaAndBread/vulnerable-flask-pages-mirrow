2420c8f1379376d24d724087b84cf62974522fdb
Nikita <nnigmat@gmail.com>
2020-02-25 14:42:11 +0300

    Add handlers

--- app.py
+++ app.py
@@ -11,7 +11,42 @@ def index():
 
 
 @app.route('/task1')
-def page1():
+def task1():
+    return render_template('task1.html')
+
+
+@app.route('/task2')
+def task2():
+    return render_template('task1.html')
+
+
+@app.route('/task3')
+def task3():
+    return render_template('task1.html')
+
+
+@app.route('/task4')
+def task4():
+    return render_template('task1.html')
+
+
+@app.route('/task5')
+def task5():
+    return render_template('task1.html')
+
+
+@app.route('/task6')
+def task6():
+    return render_template('task1.html')
+
+
+@app.route('/task7')
+def task7():
+    return render_template('task1.html')
+
+
+@app.route('/task8')
+def task8():
     return render_template('task1.html')
 
 
