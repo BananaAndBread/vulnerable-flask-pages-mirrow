744f91ba2acb7388518cbfeaba7ac6076f44e6ec
Nikita <nnigmat@gmail.com>
2020-02-25 13:41:08 +0300

    Add index page

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 # ◕_◕ довойте нумеровать странички согласно их
 # номеру в том списке с типами уязвимостей ◕_◕
 
-@app.route('/page1')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/task1')
 def page1():
-    return render_template('page1.html')
+    return render_template('task1.html')
 
 
 if __name__ == '__main__':
