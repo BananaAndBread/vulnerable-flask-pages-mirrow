a7f0f3104bf9d36984a7a8881e9bfdc2b2b0191a
fenchelfen <m.lyamets@innopolis.ru>
2020-02-28 14:59:35 +0300

    Update csrf and deploy

--- csrf_app.py
+++ csrf_app.py
@@ -6,20 +6,22 @@ app.config['SECRET_KEY'] = 'our very hard to guess secretfir'
 is_deleted = False
 
 
-@app.route('/task1', methods=['GET', 'POST'])
+@app.route('/accounts/delete', methods=['GET', 'POST'])
 def american_bank():
     global is_deleted
 
-    if request.method == 'GET' and not is_deleted:
-        return make_response('Forbidden, you are not logged in'), 403
-
-    if request.method == 'POST' and request.cookies['sne_csrftoken'] == '1337':
+    if request.cookies.get('sne_csrftoken') == '1337':
         is_deleted = True
 
-    flag = 'flag{SNE_TASK1_ABCDEFG_1234}'
-    return make_response(f'''
-        <html>Here you go, kiddo {flag}</html>
-    ''')
+    if request.method == 'GET' and \
+            (is_deleted or request.cookies.get('sne_csrftoken') == '1337'):
+        flag = 'flag{SNE_TASK1_ABCDEFG_1234}'
+
+        return make_response(f'''
+            <html>Here you go, kiddo {flag}</html>
+        ''')
+
+    return make_response('Forbidden, you are not logged in'), 403
 
 
 if __name__ == '__main__':
--- host_start.sh
+++ host_start.sh
@@ -1,5 +1,5 @@
 docker kill chrome isenmesez csrf_isenmesez ;
 
 docker run -d --rm --network='host' --name chrome selenium/standalone-chrome:3.141.59-zirconium && \
-docker run -d --rm --network='host' --name isenmesez isenmesez:latest && \
-docker run -d --network='host' --name csrf_isenmesez csrf_isenmesez:latest
+docker run -d --rm --network='host' --name isenmesez fenchelfen/isenmesez:latest && \
+docker run -d --rm --network='host' --name csrf_isenmesez fenchelfen/csrf_isenmesez:latest
